db95d0fdf676e54f96bcd802ab7e5c6d09960461
Muhammad Huzaifa <79418234+huzaifa620@users.noreply.github.com>
2025-08-21 03:40:30 +0500

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -8,6 +8,14 @@ export const metaData = {
 const RootLayout = ({ children }: { children: React.ReactNode }) => {
   return (
     <html lang="en">
+      <head>
+        <title>{metaData.title}</title>
+        <meta name="description" content={metaData.description} />
+        <link
+          rel="icon"
+          href="data:image/svg+xml,<svg xmlns=%22http://www.w3.org/2000/svg%22 viewBox=%220 0 100 100%22><text y=%22.9em%22 font-size=%2290%22>🏎️</text></svg>"
+        />
+      </head>
       <body className="min-h-dvh bg-gradient-to-b from-neutral-50 to-neutral-200 text-neutral-800 antialiased">
         <div className="mx-auto w-full">{children}</div>
       </body>
